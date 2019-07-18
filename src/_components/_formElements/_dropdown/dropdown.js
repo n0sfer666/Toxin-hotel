7662612754be306cda55e86588056c7bed15c3e5
Khasanov Petr <pietroismad@gmail.com>
2019-07-19 00:23:24 +0300

    work on dropdown component

--- src/_components/_formElements/_dropdown/dropdown.js
+++ src/_components/_formElements/_dropdown/dropdown.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
- console.log('well done');
+    var adults,
+        children,
+        babies,
+        bedrooms,
+        beds,
+        bathrooms,
+        flagChanger,
+        btnClass,
 });
