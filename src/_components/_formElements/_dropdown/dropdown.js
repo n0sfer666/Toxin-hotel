0251a00a29859cc0730f0825857c303366165982
Khasanov Petr <pietroismad@gmail.com>
2019-07-19 23:54:08 +0300

    keep working on dropdown.js + some fixs

--- src/_components/_formElements/_dropdown/dropdown.js
+++ src/_components/_formElements/_dropdown/dropdown.js
@@ -1,10 +1,82 @@
 $(document).ready(function() {
-    var adults,
-        children,
-        babies,
-        bedrooms,
-        beds,
-        bathrooms,
-        flagChanger,
-        btnClass,
+    // variables
+        // 
+    var main = {
+            guests: "#_dropdownGuests",
+            rooms: "#_dropdownRooms"
+        },
+        link = {
+            guests: "#_dropdownGuests__link",
+            rooms: "#_dropdownRooms__link"
+        },
+        text = {
+            guests: "#_dropdownGuests__text",
+            rooms: "#_dropdownRooms__text"
+        },
+        container = {
+            guests: "#_dropdownGuests__container",
+            rooms: "#_dropdownRooms__container"
+        },
+        flag = {
+            guests: false,
+            rooms: false
+        },
+        cleanLink = {
+            guests: "#_dropdownGuests__cleanLink",
+            rooms: "#_dropdownRooms__cleanLink"
+        },
+        applyLink = {
+            guests: "#_dropdownGuests__applyLink",
+            rooms: "#_dropdownRooms__applyLink"
+        },
+        // 
+        btnPlus = {
+            adults: "#adultsPlus",
+            children: "#childrenPlus",
+            babies: "#babiesPlus",
+            bedrooms: "#bedroomsPlus",
+            beds: "#bedsPlus",
+            bathrooms: "#bathroomsPlus"
+        },
+        btnMinus = {
+            adults: "#adultsMinus",
+            children: "#childrenMinus",
+            babies: "#babiesMinus",
+            bedrooms: "#bedroomsMinus",
+            beds: "#bedsMinus",
+            bathrooms: "#bathroomsMinus"
+        },
+        counter = {
+            adults: "#adultsCounter",
+            children: "#childrenCounter",
+            babies: "#babiesCounter",
+            bedrooms: "#bedroomsCounter",
+            beds: "#bedsCounter",
+            bathrooms: "#bathroomsCounter"
+        },
+        amount = {
+            adults: "#adultsAmount",
+            children: "#childrenAmount",
+            babies: "#babiesAmount",
+            bedrooms: "#bedroomsAmount",
+            beds: "#bedsAmount",
+            bathrooms: "#bathroomsAmount"
+        },
+        // 
+        btnClass = {
+            normal: "buttons__plusMinus",
+            active: "buttons__plusMinus_active"
+        },
+        mainClass = {
+            normal: "dropdown",
+            active: "dropdown_active"
+        },
+        cleanLinkClass = {
+            normal: "dropdown__applyLink",
+            active: "dropdown__applyLink_active"
+        };
+    // magic
+    
+    // functions
+    // $(element).removeClass(classRm).addClass(classAdd);
 });
