02f2aa4ef9286a8a7e7674d8bd9c8f2bede9b6b6
Khasanov Petr <pietroismad@gmail.com>
2019-07-24 19:34:42 +0300

    fix little mistake

--- src/_components/_formElements/_dropdown/dropdown.js
+++ src/_components/_formElements/_dropdown/dropdown.js
@@ -174,7 +174,6 @@ function countBabies() {
         return ', ' + amount.babies + ' младенцев';
     }
 }
-function countRoom
 })
 capitalize = function(str1){
     return str1.charAt(0).toUpperCase() + str1.slice(1);
