5b2e32fa0b175bdc7695b672f1d6e3633b56fe72
Khasanov Petr <pietroismad@gmail.com>
2019-07-28 19:03:47 +0300

    datepicker.js is done. Need to move to export module from index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import IMask from 'imask';
 $(document).ready(function() {
     var element1 = document.getElementById('maskedTextField');
     var element2 = document.getElementById('dateDropdownLeft');
+    var element3 = document.getElementById('dateDropdownRight');
     var maskOptions = {
         mask: 'd.m.y',
         lazy: true,  // make placeholder always visible
@@ -30,48 +31,55 @@ $(document).ready(function() {
         }
     };
     var mask1 = IMask(element1, maskOptions);    
-    var mask2 = IMask(element2, maskOptions);   
+    // var mask2 = IMask(element2, maskOptions);   
+    // var mask3 = IMask(element3, maskOptions);
 });
 // add dropdown (guests and romms) script
 import './_components/_formElements/_dropdown/dropdown';
 // add datepicker
 import 'air-datepicker';
 $(document).ready(function() {
-    $('#dateDropdownLeft').datepicker({
-        language: {
-            days: ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'],
-            daysShort: ['Вос','Пон','Вто','Сре','Чет','Пят','Суб'],
-            daysMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
-            months: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
-            monthsShort: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек'],
-            today: 'Сегодня',
-            clear: 'Очистить',
-            dateFormat: 'dd.mm.yyyy',
-            timeFormat: 'hh:ii',
-            firstDay: 1
+    function getValue (DateText) {
+        var day = DateText.getDate();
+        var month = DateText.getMonth();
+        var year =  DateText.getFullYear();
+        day = (day < 10) ? ("0" + day) : day ;
+        month = (month < 10) ? ("0" + month) : month ;
+        return (day + "." + month + "." + year);
+    };
+    let datepickerWrapLeft = $('#dateDropdownLeft').datepicker({
+        language: "ru",
+        minDate: new Date(),
+        range: true,
+        multipleDays: 2,
+        inline: false,
+        dateFormat: "dd.mm.yyyy",
+        navTitles: {
+            days: 'MM yyyy'
         },
-            minDate: new Date(),
-            navTitles: {
-                days: 'MM yyyy'
-            },
-            clearButton: true,
-            onShow: function (dp, animationCompleted) {
-                if (!animationCompleted) {
-                   if (dp.$datepicker.find('button').html()===undefined) { /*ONLY when button don't existis*/
-                      dp.$datepicker.append('<button type="button" class="uk-button uk-button-default uk-button-small uk-width-1-1 uk-margin-small-bottom" disabled="disabled"><i class="fas fa-check"></i> Ready</button>');
-                      dp.$datepicker.find('button').click(function(event) {
-                         dp.hide();
-                      });
-                   }
-                }
-             },
-             onSelect: function (formattedDate, date, dp) {
-                if (formattedDate.length>0) {
-                   dp.$datepicker.find('button').prop('disabled', false).removeClass('uk-button-default').addClass('uk-button-primary');
-                } else {
-                   dp.$datepicker.find('button').prop('disabled', true).removeClass('uk-button-primary').addClass('uk-button-default');
-                }
-             }
-    });
+        onSelect: function(fd, startDateText, endDateText) {
+            startDateText = datepickerWrapLeft.selectedDates[0];
+            endDateText = datepickerWrapLeft.selectedDates[1];
+            $("#dateDropdownLeft").val(getValue(startDateText));
+            $("#dateDropdownRight").val(endDateText.getDate() + "." + endDateText.getMonth() + "." + endDateText.getFullYear());
+        }
+    }).data('datepicker');
+    let datepickerWrapRight = $('#dateDropdownRight').datepicker({
+        language: "ru",
+        minDate: new Date(),
+        range: true,
+        multipleDays: 2,
+        inline: false,
+        formatDate: "dd.mm.yyyy",
+        navTitles: {
+            days: 'MM yyyy'
+        },
+        onSelect: function(fd, startDateText, endDateText) {
+            startDateText = datepickerWrapRight.selectedDates[0];
+            endDateText = datepickerWrapRight.selectedDates[1];
+            $("#dateDropdownLeft").val(startDateText.getDate() + "." + startDateText.getMonth() + "." + startDateText.getFullYear());
+            $("#dateDropdownRight").val(endDateText.getDate() + "." + endDateText.getMonth() + "." + endDateText.getFullYear());
+        }
+    }).data('datepicker');
 
 });
