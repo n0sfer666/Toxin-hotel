c8033bdbb5aa31c7ee004cde1c95a36c3a9ea443
Khasanov Petr <pietroismad@gmail.com>
2019-09-09 17:02:55 +0300

    datepicker is full complete. FormElements full complete

--- src/_components/_formElements/_dateDropdown/_dateDropdown.js
+++ src/_components/_formElements/_dateDropdown/_dateDropdown.js
@@ -14,6 +14,9 @@ $(document).ready(function() {
         range: true,
         multipleDays: 2,
         inline: false,
+        prevHtml: "<svg width='17' height='18' viewBox='0 0 17 18' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M16.1755 8.01562V9.98438H3.98801L9.56613 15.6094L8.15988 17.0156L0.144258 9L8.15988 0.984375L9.56613 2.39062L3.98801 8.01562H16.1755Z' fill='#BC9CFF'/></svg>",
+        nextHtml: "<svg width='17' height='18' viewBox='0 0 17 18' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M8.36301 0.984375L16.3786 9L8.36301 17.0156L6.95676 15.6094L12.5349 9.98438H0.347383V8.01562H12.5349L6.95676 2.39062L8.36301 0.984375Z' fill='#BC9CFF'/></svg>",
+        toggleSelected: true,
         dateFormat: "dd.mm.yyyy",
         navTitles: {
             days: 'MM yyyy'
@@ -23,7 +26,23 @@ $(document).ready(function() {
             endDateText = datepickerWrapLeft.selectedDates[1];
             $("#dateDropdownLeft").val(getValue(startDateText));
             $("#dateDropdownRight").val(getValue(endDateText));
-        }
+        },
+        onShow: function (dp, animationCompleted) {
+            if (!animationCompleted) {
+               if (dp.$datepicker.find('h3').html()===undefined) { /*ONLY when button don't existis*/
+                  dp.$datepicker.append('<button type="button" class="datepicker__clearLink">Очистить</button>');
+                  dp.$datepicker.append('<h3 href="#" class="datepicker__applyLink">Применить</h3>');
+                  dp.$datepicker.find('h3').click(function(event) {
+                     dp.hide();
+                  });
+                  dp.$datepicker.find('button').click(function(event) {
+                    $("#dateDropdownLeft").val("");
+                    $("#dateDropdownRight").val("");
+                    datepickerWrapLeft.clear();
+                 });
+               }
+            }
+         }
     }).data('datepicker');
     let datepickerWrapRight = $('#dateDropdownRight').datepicker({
         language: "ru",
@@ -40,6 +59,22 @@ $(document).ready(function() {
             endDateText = datepickerWrapRight.selectedDates[1];
             $("#dateDropdownLeft").val(getValue(startDateText));
             $("#dateDropdownRight").val(getValue(endDateText));
-        }
+        },
+        onShow: function (dp, animationCompleted) {
+            if (!animationCompleted) {
+               if (dp.$datepicker.find('h3').html()===undefined) { /*ONLY when button don't existis*/
+                  dp.$datepicker.append('<button type="button" class="datepicker__clearLink">Очистить</button>');
+                  dp.$datepicker.append('<h3 href="#" class="datepicker__applyLink">Применить</h3>');
+                  dp.$datepicker.find('h3').click(function(event) {
+                     dp.hide();
+                  });
+                  dp.$datepicker.find('button').click(function(event) {
+                    $("#dateDropdownLeft").val("");
+                    $("#dateDropdownRight").val("");
+                    datepickerWrapLeft.clear();
+                 });
+               }
+            }
+         }
     }).data('datepicker');
 });
--- src/_components/_formElements/_filterDateDropdown/_filterDateDropdown.js
+++ src/_components/_formElements/_filterDateDropdown/_filterDateDropdown.js
@@ -10,6 +10,21 @@ $(document).ready(function(){
         dateFormat: "dd.mm.yyyy",
         navTitles: {
             days: 'MM yyyy'
-        }        
+        },
+        onShow: function (dp, animationCompleted) {
+            if (!animationCompleted) {
+               if (dp.$datepicker.find('h3').html()===undefined) { /*ONLY when button don't existis*/
+                  dp.$datepicker.append('<button type="button" class="datepicker__clearLink">Очистить</button>');
+                  dp.$datepicker.append('<h3 href="#" class="datepicker__applyLink">Применить</h3>');
+                  dp.$datepicker.find('h3').click(function(event) {
+                     dp.hide();
+                  });
+                  dp.$datepicker.find('button').click(function(event) {
+                    $("#filterDateDropdown").val("");
+                    datepickerWrapLeft.clear();
+                 });
+               }
+            }
+         }      
     }).data('datepicker');
 })
