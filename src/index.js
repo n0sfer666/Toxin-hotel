eed924ecde11c8b6369a362f7927d12b5e2fdb19
Khasanov Petr <pietroismad@gmail.com>
2019-09-10 19:55:05 +0300

    little fix before the next sub-level

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 // add css
 import './main.scss';
-// add revew_image1.png
-import './_components/_formElements/_review/review_image1.png';
-// add masked text field
-import './_components/_formElements/_maskedTextField/_maskedTextField';
-// add dropdown (guests and romms) script
-import './_components/_formElements/_dropdown/dropdown';
-// add date dropdown
-import './_components/_formElements/_dateDropdown/_dateDropdown';
-//add filter date dropdown
-import './_components/_formElements/_filterDateDropdown/_filterDateDropdown';
-//add expandable checkbox list
-import './_components/_formElements/_expandableCheckboxList/_expandableCheckboxList';
-// add listener for like buttons
-import './_components/_formElements/_buttons/_like/_buttons__like';
-//  add listener for rate buttons
-import './_components/_formElements/_buttons/_rate/_buttons_rate';
-// add range slider
-import './_components/_formElements/_range_slider/_range_slider';
-//add pagination
-import './_components/_formElements/_pagination/_pagination';
+// // add revew_image1.png
+// import './_components/_formElements/_review/review_image1.png';
+// // add masked text field
+// import './_components/_formElements/_maskedTextField/_maskedTextField';
+// // add dropdown (guests and romms) script
+// import './_components/_formElements/_dropdown/dropdown';
+// // add date dropdown
+// import './_components/_formElements/_dateDropdown/_dateDropdown';
+// //add filter date dropdown
+// import './_components/_formElements/_filterDateDropdown/_filterDateDropdown';
+// //add expandable checkbox list
+// import './_components/_formElements/_expandableCheckboxList/_expandableCheckboxList';
+// // add listener for like buttons
+// import './_components/_formElements/_buttons/_like/_buttons__like';
+// //  add listener for rate buttons
+// import './_components/_formElements/_buttons/_rate/_buttons_rate';
+// // add range slider
+// import './_components/_formElements/_range_slider/_range_slider';
+// //add pagination
+// import './_components/_formElements/_pagination/_pagination';
