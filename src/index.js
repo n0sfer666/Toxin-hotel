45715e36b5d3a4bdae782223e7da3c92beb8508f
Khasanov Petr <pietroismad@gmail.com>
2019-09-24 21:09:54 +0300

    registration is complete + fix border, toggle buttons and textField

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 // add css
 import './main.scss';
-import './_components/_formElements/_dateDropdown/_dateDropdown';
-import './_components/_formElements/_dropdown/dropdown';
+// maskedTextFIeld - birthday
+import './_components/_cards/_registration/_registration';
