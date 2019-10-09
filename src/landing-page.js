62591f586fa4a2067a9b731fc6fe9bbd66e91c00
Khasanov Petr <pietroismad@gmail.com>
2019-10-09 21:06:15 +0300

    registration is complete

--- src/landing-page.js
+++ src/landing-page.js
@@ -1,4 +1,3 @@
 import './landing-page.scss';
-import './img/landing-main.png';
 import './img/landing.png';
 import './_components/_cards/_findApart/_findApart';
--- src/registration.js
+++ src/registration.js
@@ -1,1 +1,2 @@
 import './registration.scss';
+import './_components/_cards/_registration/_registration';
