30ce4d9e54e649d8fc52fce1114509412e1ee6fe
Khasanov Petr <pietroismad@gmail.com>
2019-10-16 20:35:15 +0300

    search-room is complete

--- src/pages/search-room/search-room.js
+++ src/pages/search-room/search-room.js
@@ -4,35 +4,34 @@ import '../../_components/_formElements/_dateDropdown/_filterDateDropdown/_filte
 import '../../_components/_formElements/_range_slider/_range_slider';
 import '../../_components/_formElements/_expandableCheckboxList/_expandableCheckboxList';
 import '../../_components/_cards/_apart/_apart';
+
+
 import 'paginationjs';
 $(document).ready(function() {
+    var testA = $('div[class="apart__container"');
     var array = [];
-    for(var i=0; i<200; i++)
-        array.push(i);
+    var elementsOnPage = 12;
+    for(var j=0; j < 8; j++)
+        for(var i=0; i<testA.length; i++)
+            array.push(testA[i]);
     $('#searchRoom__pagination').pagination({
         dataSource: array,
+        // locator: "",
         showPrevious: false,
         nextText: "",
         pageRange: 1,
-        pageSize: 12,
+        pageSize: elementsOnPage,
         showNavigator: true,
-        formatNavigator: '<br><br><br><%= currentPage %>, <%= totalPage %> pages, <%= totalNumber %> entries',
-        // callback: function(data, pagination) {
-        //     // template method of yourself
-        //     var html = simpleTemplating(data);
-        //     $('#searchRoom__data').html(html);
-        // }
+        formatNavigator: function(currentPage, totalPage, totalNumber) {
+            var first= elementsOnPage * currentPage - (elementsOnPage - 1), 
+                last = elementsOnPage * currentPage, 
+                total= totalNumber > 100 ? "100+" : totalNumber;
+            return String(first + " - " + last + " из " + total + " вариантов аренды");
+        },
+        callback: function(data, pagination) {
+            // template method of yourself
+            var html = data;
+            $('#searchRoom__data').html(html);
+        }
     });
-    // function simpleTemplating(data) {
-    //     var html = '<ul>';
-    //     $.each(data, function(index, item){
-    //         html += '<li>'+ item +'</li>';
-    //     });
-    //     html += '</ul>';
-    //     return html;
-    // }
-
-    var testA = $('div[class="apart__container"');
-    $("#test").html(testA[0]);
-
 })
