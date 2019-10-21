4926c56ea55c5848d178fe25e1f666421f53c69e
Khasanov Petr <pietroismad@gmail.com>
2019-10-21 23:59:04 +0300

    Form Elements page is responsive

--- src/_components/_formElements/_pagination/_pagination.js
+++ src/_components/_formElements/_pagination/_pagination.js
@@ -1,6 +1,7 @@
 import 'paginationjs';
 $(document).ready(function() {
     var array = [];
+    var elementsOnPage = 12;
     for(var i=0; i<200; i++)
         array.push(i);
     $('#pagination').pagination({
@@ -8,8 +9,15 @@ $(document).ready(function() {
         showPrevious: false,
         nextText: "",
         pageRange: 1,
-        pageSize: 12,
+        pageSize: elementsOnPage,
         showNavigator: true,
-        formatNavigator: '<%= currentPage %>, <%= totalPage %> pages, <%= totalNumber %> entries'
+        formatNavigator: function(currentPage, totalPage, totalNumber) {
+            var first= elementsOnPage * currentPage - (elementsOnPage - 1), 
+                last = elementsOnPage * currentPage, 
+                total= totalNumber > 100 ? "100+" : totalNumber;
+                if(last>totalNumber)
+                    last = totalNumber;
+            return String(first + " - " + last + " из " + total + " вариантов аренды");
+        }
     })
 })
