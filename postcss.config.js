24f970f565993a9c6828e794558996fb02b3b594
Khasanov Petr <pietroismad@gmail.com>
2020-02-26 13:20:24 +0300

    refactor(postcss.config): added postcss-discard-comments

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    plugins: [
-      require('autoprefixer'),
-      // require('cssnano')
-    ]
+  plugins: {
+    'autoprefixer': {},
+    'postcss-discard-comments': {},
+    // 'cssnano': {}
   }
+}
