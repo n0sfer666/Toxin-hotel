0acd89a955263c82aaf74e0a0715aa567dae5061
Khasanov Petr <pietroismad@gmail.com>
2020-02-28 17:24:43 +0300

    refactor(project): webpack.config and files structure

--- webpack.config.js
+++ webpack.config.js
@@ -3,99 +3,66 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+const pages = [
+	{
+		name: "test",
+		path: "src/pages/test/test"
+	}
+]
+const entry = {};
+const plugins = [];
+
+plugins.push(
+	new MiniCssExtractPlugin({
+		filename: '[name].css',
+		chunkFilename: '[id].css'
+	}),
+	new CopyWebpackPlugin([
+		{ from: 'src/components/cards/apart/img', to: 'img' }
+	]),
+	new Webpack.ProvidePlugin({
+		$: 'jquery',
+		jQuery: 'jquery',
+		'windows.jQuery': 'jquery'
+	}),
+	new Webpack.ProvidePlugin({
+		noUiSlider: 'nouislider'
+	}),
+	new Webpack.ProvidePlugin({
+		wNumb: 'wnumb'
+	})
+)
+
+pages.forEach( (page) => {
+	entry[page.name] = './' + page.path + '.js';
+	plugins.push(new HtmlWebpackPlugin({
+		filename: page.name + '.html',
+		template: page.path + '.pug',
+		inject: false
+	}))
+})
+
+
+
 module.exports = {
-	entry: {
-		index: "./src/pages/index/index.js",
-		colorsTypes: "./src/pages/colors-types/colors-types.js",
-		elements: "./src/pages/elements/elements.js",
-		cards: "./src/pages/cards/cards.js",
-		headersFooters: "./src/pages/headers-footers/headers-footers.js",
-		landingPage: "./src/pages/landing-page/landing-page.js",
-		registration: "./src/pages/registration/registration.js",
-		signin: "./src/pages/sign-in/sign-in.js",
-		roomDetail: "./src/pages/room-detail/room-detail.js",
-		searchRoom: "./src/pages/search-room/search-room.js"
-	},
+	entry: entry,
+
 	output: {
 		filename: '[name].js',
 		path: __dirname + '/docs'
-	}   ,
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: "[name].css",
-			chunkFilename: "[id].css"
-		}),
-		new CopyWebpackPlugin([
-			{from:'src/_components/_cards/_apart/img', to:'img'} 
-		]),
-		new HtmlWebpackPlugin({
-			filename: 'index.html',
-			template: 'src/pages/index/index.pug',
-			inject: false
-		}),
-		new HtmlWebpackPlugin({
-			filename: 'colors-types.html',
-			template: 'src/pages/colors-types/colors-types.pug',
-			inject: false
-		}),
-		new HtmlWebpackPlugin({
-			filename: 'elements.html',
-			template: 'src/pages/elements/elements.pug',
-			inject: false
-		}),
-		new HtmlWebpackPlugin({
-			filename: 'cards.html',
-			template: 'src/pages/cards/cards.pug',
-			inject: false
-		}),
-		new HtmlWebpackPlugin({
-			filename: 'headers-footers.html',
-			template: 'src/pages/headers-footers/headers-footers.pug',
-			inject: false
-		}),
-		new HtmlWebpackPlugin({
-			filename: 'landing-page.html',
-			template: 'src/pages/landing-page/landing-page.pug',
-			inject: false
-		}),
-		new HtmlWebpackPlugin({
-			filename: 'registration.html',
-			template: 'src/pages/registration/registration.pug',
-			inject: false
-		}),
-		new HtmlWebpackPlugin({
-			filename: 'sign-in.html',
-			template: 'src/pages/sign-in/sign-in.pug',
-			inject: false
-		}),
-		new HtmlWebpackPlugin({
-			filename: 'room-detail.html',
-			template: 'src/pages/room-detail/room-detail.pug',
-			inject: false
-		}),
-		new HtmlWebpackPlugin({
-			filename: 'search-room.html',
-			template: 'src/pages/search-room/search-room.pug',
-			inject: false
-		}),
-		new Webpack.ProvidePlugin({
-			$: 'jquery',
-			jQuery: 'jquery',
-			'window.jQuery': 'jquery'
-		}),
-		new Webpack.ProvidePlugin({
-			noUiSlider: 'nouislider'
-		}),
-		new Webpack.ProvidePlugin({
-			wNumb: 'wnumb'
-		})
-	],
+	},
+
+	plugins: plugins,
+
+	devtool: 'inline-source-map',
+
 	devServer: {
 		clientLogLevel: 'error',
 		contentBase: __dirname + '/docs',
 		compress: true,
 		port: 9090 
 	},
+	
 	module: {
 		rules: [
 			{
