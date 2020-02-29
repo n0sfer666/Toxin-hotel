56e0a5f5ffd52a6dbfcd1cf7f63bd2debb680cf4
Khasanov Petr <pietroismad@gmail.com>
2020-02-29 11:01:48 +0300

    refactor(masked-text-field): make the export variable maskOptions

---
+++ src/components/_formElements/text-field/masked-text-field/mask-options.js
@@ -0,0 +1,25 @@
+let maskOptions = {
+	mask: 'd.m.y',
+	lazy: true,  // make placeholder always visible
+
+	blocks: {
+		y: {
+			mask: IMask.MaskedRange,
+			from: 1900,
+			to:2020
+		},
+
+		m: {
+			mask: IMask.MaskedRange,
+			from: 1,
+			to: 12
+		},
+
+		d: {
+			mask: IMask.MaskedRange,
+			from: 1,
+			to: 31
+		},
+	}
+}
+export { maskOptions }
