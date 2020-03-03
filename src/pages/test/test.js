bca019b0f6ffae3652ed0f6bb335eb2ff179f0c3
Khasanov Petr <pietroismad@gmail.com>
2020-03-03 11:10:19 +0300

    refactor(date-dropdown): tab size, js rewrited, css cleaned

--- src/pages/test/test.js
+++ src/pages/test/test.js
@@ -1,3 +1,19 @@
 import './test.scss';
 
-import '../../components/_formElements/expandable-checkbox-list/expandable-checkbox-list.js';
+import {getFormattedDate, datepickerConfig} from '../../components/_formElements/date-dropdown/date-dropdown.js';
+
+$(document).ready(function() {
+  let $datepickerLeft = 
+    $('.js-date-dropdown-left')
+      .datepicker(datepickerConfig)
+      .data('datepicker');
+  let $datepickerRight = 
+    $('.js-date-dropdown-right')
+      .datepicker(datepickerConfig)
+      .data('datepicker');
+  let $datepickerFilter = 
+    $('.js-date-dropdown-filter')
+      .datepicker(datepickerConfig)
+      .data('datepicker');
+
+})
