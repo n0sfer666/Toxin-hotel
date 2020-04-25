5a8cb643a57e8b8374cae8e9873051499c9486c3
Khasanov Petr <pietroismad@gmail.com>
2020-04-25 22:05:37 +0300

    refactored index.js #4

--- src/index.js
+++ src/index.js
@@ -2,9 +2,5 @@ function importAll (r) {
   r.keys().forEach(r);
 }
 importAll(require.context('./styles/', false, /\.scss$/));
-importAll(require.context('./components/', true, /\.scss$/));
-importAll(require.context('./components/', true, /\.js$/));
-importAll(require.context('./pages/', true, /\.js$/));
-importAll(require.context('./pages/', true, /\.scss$/));
-importAll(require.context('./components/', true, /\.(jpeg|jpg|png)$/));
-importAll(require.context('./pages/', true, /\.(jpeg|jpg|png)$/));
+importAll(require.context('./components/', true, /\.(scss|js|jpeg|jpg|png)$/));
+importAll(require.context('./pages/', true, /\.(js|scss|jpeg|jpg|png)$/));
