3072f21e5999211890930b1e78423e13c7cd5d3e
Khasanov Petr <pietroismad@gmail.com>
2020-06-14 19:46:43 +0300

    refactored booking.js #17

--- src/components/booking/booking.js
+++ src/components/booking/booking.js
@@ -6,28 +6,7 @@ class Booking {
     this.item = item;
     this.index = index;
 
-    this.priceElement = this.getElementByClass('.js-booking__price');
-    this.priceValue = Number(this.priceElement.get(0).innerText);
-    $.each(this.priceElement, (key, item) => {
-      item.innerText = this.getStringWithSeparate(item.innerText);
-    })
-
-    this.daysInElement = this.getElementByClass('.js-booking__daysIn');
-
-    this.costElement = this.getElementByClass('.js-booking__cost');
-
-    this.discountElement = this.getElementByClass('.js-booking__discount');
-    this.discountValue = Number(this.discountElement.get(0).innerText);
-    $.each(this.discountElement, (key, item) => {
-      item.innerText = this.getStringWithSeparate(item.innerText);
-    })
-
-    this.additionalElement = this.getElementByClass('.js-booking__additional');
-
-    this.complementaryElement = this.getElementByClass('.js-booking__complementary');
-    this.complementaryValue = Number(this.complementaryElement.get(0).innerText);
-
-    this.totalElement = this.getElementByClass('.js-booking__total');
+    this.initiate();
   }
 
   getElementByClass(selector) {
@@ -58,6 +37,31 @@ class Booking {
     let total = cost - this.discountValue + this.complementaryValue;
     this.totalElement.text(`${this.getStringWithSeparate(total)}`);
     
+  }
+
+  initiate() {
+    this.priceElement = this.getElementByClass('.js-booking__price');
+    this.priceValue = Number(this.priceElement.get(0).innerText);
+    $.each(this.priceElement, (key, item) => {
+      item.innerText = this.getStringWithSeparate(item.innerText);
+    })
+
+    this.daysInElement = this.getElementByClass('.js-booking__daysIn');
+
+    this.costElement = this.getElementByClass('.js-booking__cost');
+
+    this.discountElement = this.getElementByClass('.js-booking__discount');
+    this.discountValue = Number(this.discountElement.get(0).innerText);
+    $.each(this.discountElement, (key, item) => {
+      item.innerText = this.getStringWithSeparate(item.innerText);
+    })
+
+    this.additionalElement = this.getElementByClass('.js-booking__additional');
+
+    this.complementaryElement = this.getElementByClass('.js-booking__complementary');
+    this.complementaryValue = Number(this.complementaryElement.get(0).innerText);
+
+    this.totalElement = this.getElementByClass('.js-booking__total');
   }
 }
 
