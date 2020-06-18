141726955c02fce4fe5bfdfe58b7301767b79c4d
Khasanov Petr <pietroismad@gmail.com>
2020-06-18 19:19:41 +0300

    pulled handler into separate meyhod #17

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -4,8 +4,9 @@ class Dropdown {
   constructor(item, index) {
     this.container = item;
     this.index = index;
-    this.config = this.getConfig();
 
+    this.config = this.getConfig();
+    
     this.$instance = this.getInstance();
 
     this.$clearButton = this.$instance.find('.js-clear-dropdown-button').hide();
@@ -35,106 +36,114 @@ class Dropdown {
   }
 
   getConfig() {
-    return {
-      setSelectionText: (itemCount, totalItems) => {
-        let text = '';
+    const config = {};
+    const onChange = this.onChange.bind(this);
 
-        const isGuests = (
-          itemCount.adults !== undefined
-          || itemCount.babies !== undefined
-          || itemCount.children !== undefined
-        );
-        const isRooms = (
-          itemCount.bedrooms !== undefined
-          || itemCount.beds !== undefined
-          || itemCount.bathrooms !== undefined
-        );
+    config.setSelectionText = this.setSelectionText;
+    config.onChange = onChange;
 
-        if (totalItems === 0) {
-          if (isGuests) {
-            text = 'Сколько гостей';
-          }
-        }
-
-        const guests = itemCount.adults + itemCount.children;
-        const { babies } = itemCount;
-
-        const { bedrooms } = itemCount;
-        const { beds } = itemCount;
-
-        if (guests === 1) {
-          text = `${guests} гость`;
-        }
-        if (guests > 1 && guests < 5) {
-          text = `${guests} гостя`;
-        }
-        if (guests >= 5) {
-          text = `${guests} гостей`;
-        }
-        if (babies === 1) {
-          text += guests > 0
-            ? `, ${babies} младенец`
-            : `${babies} младенец`;
-        }
-        if (babies > 1 && babies < 5) {
-          text += guests > 0
-            ? `, ${babies} младенца`
-            : `${babies} младенца`;
-        }
-        if (babies >= 5) {
-          text += guests > 0
-            ? `, ${babies} младенцев`
-            : `${babies} младенцев`;
-        }
-
-        if (bedrooms === 1) {
-          text = `${bedrooms} спальня`;
-        }
-        if (bedrooms > 1 && bedrooms < 5) {
-          text = `${bedrooms} спальни`;
-        }
-        if (bedrooms >= 5) {
-          text = `${bedrooms} спален`;
-        }
-        if (beds === 1) {
-          text += bedrooms !== 0
-            ? `, ${beds} кровать...`
-            : `${beds} кровать...`;
-        }
-        if (beds > 1 && beds < 5) {
-          text += bedrooms !== 0
-            ? `, ${beds} кровати...`
-            : `${beds} кровати...`;
-        }
-        if (beds >= 5) {
-          text += bedrooms !== 0
-            ? `, ${beds} кроватей...`
-            : `${+beds} кроватей...`;
-        }
-
-        return text;
-      },
-      onChange: (id, count, totalItems) => {
-        if (count > 0) {
-          $(`[data-id=${id}]`).find('.button-decrement')
-            .removeClass('button-decrement')
-            .addClass('button-decrement_actived');
-        } else {
-          $(`[data-id=${id}]`).find('.button-decrement_actived')
-            .removeClass('button-decrement_actived')
-            .addClass('button-decrement');
-        }
-        if (totalItems === 0) {
-          this.$clearButton.hide();
-        } else {
-          this.$clearButton.show();
-        }
-      },
-    };
+    return config;
   }
 
   getInstance() {
     return $(this.container).iqDropdown(this.config);
+  }
+
+  onChange(id, count, totalItems) {
+    if (count > 0) {
+      $(`[data-id=${id}]`).find('.button-decrement')
+        .removeClass('button-decrement')
+        .addClass('button-decrement_actived');
+    } else {
+      $(`[data-id=${id}]`).find('.button-decrement_actived')
+        .removeClass('button-decrement_actived')
+        .addClass('button-decrement');
+    }
+    if (totalItems === 0) {
+      this.$clearButton.hide();
+    } else {
+      this.$clearButton.show();
+    }
+  }
+
+  setSelectionText(itemCount, totalItems) {
+    let text;
+
+    const isGuests = (
+      itemCount.adults !== undefined
+      || itemCount.babies !== undefined
+      || itemCount.children !== undefined
+    );
+    const isRooms = (
+      itemCount.bedrooms !== undefined
+      || itemCount.beds !== undefined
+      || itemCount.bathrooms !== undefined
+    );
+
+    if (totalItems === 0) {
+      if (isGuests) {
+        text = 'Сколько гостей';
+      }
+    }
+
+    const guests = itemCount.adults + itemCount.children;
+    const { babies } = itemCount;
+    const { bedrooms } = itemCount;
+    const { beds } = itemCount;
+
+    if (guests === 1) {
+      text = `${guests} гость`;
+    }
+    if (guests > 1 && guests < 5) {
+      text = `${guests} гостя`;
+    }
+    if (guests >= 5) {
+      text = `${guests} гостей`;
+    }
+
+    if (babies === 1) {
+      text += guests > 0
+        ? `, ${babies} младенец`
+        : `${babies} младенец`;
+    }
+    if (babies > 1 && babies < 5) {
+      text += guests > 0
+        ? `, ${babies} младенца`
+        : `${babies} младенца`;
+    }
+    if (babies >= 5) {
+      text += guests > 0
+        ? `, ${babies} младенцев`
+        : `${babies} младенцев`;
+    }
+
+    if (bedrooms === 1) {
+      text = `${bedrooms} спальня`;
+    }
+    if (bedrooms > 1 && bedrooms < 5) {
+      text = `${bedrooms} спальни`;
+    }
+    if (bedrooms >= 5) {
+      text = `${bedrooms} спален`;
+    }
+    
+    if (beds === 1) {
+      text += bedrooms !== 0
+        ? `, ${beds} кровать...`
+        : `${beds} кровать...`;
+    }
+    if (beds > 1 && beds < 5) {
+      text += bedrooms !== 0
+        ? `, ${beds} кровати...`
+        : `${beds} кровати...`;
+    }
+    if (beds >= 5) {
+      text += bedrooms !== 0
+        ? `, ${beds} кроватей...`
+        : `${+beds} кроватей...`;
+    }
+
+    return text;
   }
 }
 
